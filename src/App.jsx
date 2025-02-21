016e362800baf19d0e102507e1b3c6c3e4df3460
sajeela123 <U2486893@rts-365.dk>
2025-02-21 08:47:02 +0100

    ADD NOW

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,23 @@ import MainBrands from './pages/MainBrands';
 import Frontpage from './pages/shop-frontpage';
 import Footer from './components/Footer';
 import SingleProduct from './pages/single-product';
+import ShoppingCart from './pages/shoppingCart';
 
 function App() {
-    return (
-        <Router>
-            <div>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/main-brands" element={<MainBrands />} />
-                    <Route path="/shop-frontpage" element={<Frontpage />} />
-                    <Route path="/single-product" element={<SingleProduct />} />
-                </Routes>
-                <Footer />
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/main-brands" element={<MainBrands />} />
+          <Route path="/shop-frontpage" element={<Frontpage />} />
+          <Route path="/single-product" element={<SingleProduct />} />
+          <Route path="/ShoppingCart" element={<ShoppingCart />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
